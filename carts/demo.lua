292189b3ac571a0a172a26e19698f275db81152e
viperconsole <viperconsole@gmail.com>
2023-03-18 10:20:00 +0100

    Rhombicosidodecahedron

--- carts/demo.lua
+++ carts/demo.lua
@@ -60,7 +60,7 @@ local c2x = LW/2
 local c2y = LH/2
 local COLS= {}
 local SLICE <const> = 1600
-local COLNUM <const> = 8
+local COLNUM <const> = 64
 ----------------------
 -- tunnel
 ----------------------
@@ -253,7 +253,7 @@ function update_moire2()
     local c4=math.cos(angle4)
     local s4=math.sin(angle4)
     local hit = t*0.5 % 1
-    local rgb = hit < 0.2 and (0.2-hit) * 8 / 0.2 or 0
+    local rgb = hit < 0.2 and (0.2-hit) * COLNUM / 0.2 or 0
     for y=0,LH-1 do
         local ly=LH/2-y
         for x=0,LW-1 do
@@ -266,7 +266,7 @@ function update_moire2()
             local p2= rx2%10 < 5 and rx2 < 70 and rx2 > -70 and 1 or 0
             local p3= rx3%10 < 5 and rx3 < 70 and rx3 > -70 and 1 or 0
             local p4= rx4%10 < 5 and rx4 < 70 and rx4 > -70 and 1 or 0
-            local col = (p1 + p2 + p3 + p4 )*2+rgb
+            local col = (p1 + p2 + p3 + p4 )*COLNUM/4+rgb
             if col < 1 then
                 col=1
             elseif col > COLNUM then
@@ -305,7 +305,7 @@ function update_checkerboard()
             end
         end
     end
-    local bouncey=math.floor(LH* (1 - bounce*0.3))
+    local bouncey=math.floor((LH+1)* (1 - bounce*0.3))
     fill_pix(0,0,0)
     for y=bouncey,bouncey+5 do
         for x=0,LW-1 do
@@ -316,8 +316,8 @@ function update_checkerboard()
         local ycoef=(y-LH*0.7-1) / (bouncey-1-LH*0.7)
         local xoff = math.floor((1-ycoef)*LW/4)
         local ry=((1-ycoef)*(1-ycoef) * 8) % 2
-        local c1=ry <= 1 and 4 or 1
-        local c2=ry <= 1 and 1 or 4
+        local c1=ry <= 1 and COLNUM/2 or COLNUM/4
+        local c2=ry <= 1 and COLNUM/4 or COLNUM/2
         for x = xoff,LW-xoff do
             local rx = ((x-xoff)/(LW-2*xoff)*8) % 2
             local col = rx < 0.95 and c1 or rx > 1.05 and c2 or math.floor(c1+(c2-c1)*(rx-0.95)*10)
@@ -369,26 +369,29 @@ local function compare_tri(t1,t2)
     return t1[7]>t2[7]
 end
 
-function render_mesh(verts,tris,pos,squeezex,squeezey,rx,ry,altcol)
+function render_mesh(verts,tris,pos,squeezex,squeezey,rx,ry,altcol,mirror)
     local mx=matrix_rotate_x(rx)
     local my=matrix_rotate_y(ry)
     local tverts={}
     for i=1,#verts do
         tverts[i]=translate_to_view(verts[i],pos,squeezex,squeezey,mx,my)
+        if mirror then
+            tverts[i][2] = 190+(190-tverts[i][2])
+        end
     end
     for i=1,#tris do
         local t=tris[i]
-        local p1=tverts[t[1]]
-        local p2=tverts[t[2]]
-        local p3=tverts[t[3]]
+        local p1=tverts[t[1]+1]
+        local p2=tverts[t[2]+1]
+        local p3=tverts[t[3]+1]
         t[7]=(p1[3]+p2[3]+p3[3])/3
     end
     table.sort(tris, compare_tri)
     for i=1,#tris do
         local t=tris[i]
-        local a=tverts[t[1]]
-        local b=tverts[t[2]]
-        local c=tverts[t[3]]
+        local a=tverts[t[1]+1]
+        local b=tverts[t[2]+1]
+        local c=tverts[t[3]+1]
         local ab=v3d_sub({b[4],b[5],b[3]},{a[4],a[5],a[3]})
         local ac=v3d_sub({c[4],c[5],c[3]},{a[4],a[5],a[3]})
         local n=v3d_norm(v3d_cross(ab,ac))
@@ -399,8 +402,9 @@ function render_mesh(verts,tris,pos,squeezex,squeezey,rx,ry,altcol)
             gfx.set_active_layer(n[3] > 0 and LAYER_ICO2 or LAYER_ICO1)
             gfx.triangle(a[1],a[2],b[1],b[2],c[1],c[2],(128+127*t[4]/255)*diffuse,0,0)
         else
+            local rgb=mirror and mirror/768 or 1
             gfx.set_active_layer(n[3] > 0 and LAYER_ICO3 or LAYER_ICO4)
-            gfx.triangle(a[1],a[2],b[1],b[2],c[1],c[2],t[4]*diffuse+specular*64,t[5]*diffuse+specular*64,t[6]*diffuse+specular*64)
+            gfx.triangle(a[1],a[2],b[1],b[2],c[1],c[2],rgb*(t[4]*diffuse+specular*64),rgb*(t[5]*diffuse+specular*64),rgb*(t[6]*diffuse+specular*64))
         end
     end
 end
@@ -411,55 +415,153 @@ function update_ico()
             gfx.show_layer(layer)
         end
         ico_verts={
-            {-0.5,-0.5,-0.5},
-            {0.5,-0.5,-0.5},
-            {-0.5,0.5,-0.5},
-            {0.5,0.5,-0.5},
-            {-0.5,-0.5,0.5},
-            {0.5,-0.5,0.5},
-            {-0.5,0.5,0.5},
-            {0.5,0.5,0.5},
-            {0,0,-0.8},
-            {0,0,0.8},
-            {0,-0.8,0},
-            {0,0.8,0},
-            {-0.8,0,0},
-            {0.8,0,0}
+            {-0.980376, -0.197048, -0.005857},
+            {-0.946332, 0.150802, -0.285858},
+            {-0.935804, 0.079716, 0.34339},
+            {-0.901759, 0.427566, 0.063389},
+            {-0.812124, -0.53251, 0.23851},
+            {-0.8048, -0.555655, -0.20867},
+            {-0.767552, -0.255746, 0.587757},
+            {-0.759203, 0.553784, -0.341957},
+            {-0.749715, 0.007177, -0.661721},
+            {-0.688108, 0.168923, 0.705672},
+            {-0.662244, -0.429437, -0.614016},
+            {-0.633023, 0.731755, 0.252621},
+            {-0.562586, 0.41016, -0.71782},
+            {-0.500979, 0.571905, 0.649573},
+            {-0.490467, 0.857973, -0.152725},
+            {-0.483464, -0.835984, 0.259599},
+            {-0.47614, -0.859129, -0.18758},
+            {-0.411344, -0.388171, 0.824694},
+            {-0.387011, 0.051553, -0.920632},
+            {-0.333584, -0.732911, -0.592926},
+            {-0.331901, 0.036498, 0.942608},
+            {-0.299539, -0.385062, -0.872928},
+            {-0.245677, 0.868112, 0.431306},
+            {-0.235768, -0.746778, 0.621881},
+            {-0.172335, 0.625585, -0.760884},
+            {-0.144772, 0.43948, 0.88651},
+            {-0.127762, 0.902348, -0.411637},
+            {-0.119933, -0.991554, 0.049356},
+            {-0.103121, 0.994331, 0.02596},
+            {-0.003241, -0.266976, 0.963698},
+            {0.003241, 0.266978, -0.963697},
+            {0.11053, 0.735688, 0.668243},
+            {0.110728, -0.787329, -0.606508},
+            {0.127763, -0.902347, 0.411638},
+            {0.144773, -0.439479, -0.886509},
+            {0.172335, -0.625583, 0.760885},
+            {0.242772, -0.947178, -0.209555},
+            {0.271977, 0.571167, -0.774465},
+            {0.29954, 0.385063, 0.872928},
+            {0.31655, 0.847931, -0.425218},
+            {0.331901, -0.036497, -0.942608},
+            {0.341191, 0.939913, 0.012379},
+            {0.387011, -0.051552, 0.920633},
+            {0.473235, 0.780063, 0.409331},
+            {0.490467, -0.857972, 0.152726},
+            {0.528066, -0.712229, -0.462467},
+            {0.562111, -0.364379, -0.742468},
+            {0.562587, -0.410159, 0.717821},
+            {0.600637, 0.267692, -0.753376},
+            {0.662244, 0.429438, 0.614017},
+            {0.672757, 0.715506, -0.188282},
+            {0.749716, -0.007176, 0.661722},
+            {0.759203, -0.553783, 0.341958},
+            {0.775762, -0.623022, -0.100185},
+            {0.804801, 0.555656, 0.208671},
+            {0.830847, -0.06019, -0.553237},
+            {0.848333, 0.356899, -0.391094},
+            {0.946332, -0.1508, 0.285859},
+            {0.96289, -0.22004, -0.156284},
+            {0.980377, 0.197049, 0.005858}
         }
         ico_tris= {
-            {1,9,2, 0,0,255},
-            {2,9,4, 255,255,255},
-            {4,9,3, 0,0,255},
-            {3,9,1, 255,255,255},
-            {6,10,5, 0,0,255},
-            {8,10,6, 255,255,255},
-            {7,10,8, 0,0,255},
-            {5,10,7, 255,255,255},
-            {2,11,1, 255,255,255},
-            {6,11,2, 0,0,255},
-            {5,11,6, 255,255,255},
-            {1,11,5, 0,0,255},
-            {3,12,4, 255,255,255},
-            {4,12,8, 0,0,255},
-            {8,12,7, 255,255,255},
-            {7,12,3, 0,0,255},
-            {1,13,3, 0,0,255},
-            {3,13,7, 255,255,255},
-            {7,13,5, 0,0,255},
-            {5,13,1, 255,255,255},
-            {4,14,2, 0,0,255},
-            {8,14,4, 255,255,255},
-            {6,14,8, 0,0,255},
-            {2,14,6, 255,255,255},
+            {30, 24, 37},
+            {14, 28, 26},
+            {11, 13, 22},
+            {25, 38, 31},
+            {48, 56, 55},
+            {59, 57, 58},
+            {39, 41, 50},
+            {43, 49, 54},
+            {40, 46, 34},
+            {45, 36, 32},
+            {53, 52, 44},
+            {51, 42, 47},
+            {21, 19, 10},
+            {27, 15, 16},
+            {33, 35, 23},
+            {29, 20, 17},
+            {18, 8, 12},
+            {1, 3, 7},
+            {5, 4, 0},
+            {6, 9, 2},
+            {18, 12, 24, 30},
+            {24, 26, 39, 37},
+            {26, 28, 41, 39},
+            {11, 22, 28, 14},
+            {13, 25, 31, 22},
+            {31, 38, 49, 43},
+            {30, 37, 48, 40},
+            {56, 59, 58, 55},
+            {58, 57, 52, 53},
+            {50, 54, 59, 56},
+            {41, 43, 54, 50},
+            {49, 38, 42, 51},
+            {48, 55, 46, 40},
+            {46, 45, 32, 34},
+            {36, 44, 33, 27},
+            {53, 44, 36, 45},
+            {57, 51, 47, 52},
+            {47, 42, 29, 35},
+            {34, 32, 19, 21},
+            {19, 16, 5, 10},
+            {16, 15, 4, 5},
+            {33, 23, 15, 27},
+            {35, 29, 17, 23},
+            {17, 20, 9, 6},
+            {21, 10, 8, 18},
+            {8, 1, 7, 12},
+            {7, 3, 11, 14},
+            {0, 2, 3, 1},
+            {4, 6, 2, 0},
+            {9, 20, 25, 13},
+            {12, 7, 14, 26, 24},
+            {22, 31, 43, 41, 28},
+            {37, 39, 50, 56, 48},
+            {54, 49, 51, 57, 59},
+            {55, 58, 53, 45, 46},
+            {52, 47, 35, 33, 44},
+            {32, 36, 27, 16, 19},
+            {23, 17, 6, 4, 15},
+            {10, 5, 0, 1, 8},
+            {2, 9, 13, 11, 3},
+            {18, 30, 40, 34, 21},
+            {20, 29, 42, 38, 25}
         }
+        for i=#ico_tris,1,-1 do
+            local t=ico_tris[i]
+            if #t > 3 then
+                local blue=#t == 4 and 0 or #t==5 and 128 or 255
+                table.remove(ico_tris,i)
+                for j=3,#t do
+                    table.insert(ico_tris,{t[1],t[j-1],t[j], blue,blue,255})
+                end
+            else
+                t[4] = 255
+                t[5] = 255
+                t[6] = 255
+            end
+        end
         pos1[2]=-3
     end
     if t < 10 then
         acc=acc+0.001
         pos1[2] = pos1[2]+acc
-        if pos1[2] > 0.85 then
+        if pos1[2] > 0.5 then
             acc=-0.07
-            pos1[2]=0.85
+            pos1[2]=0.5
             squeeze_amount=0.5
             squeeze_t=t
         end
@@ -494,6 +596,9 @@ function render_ico()
     end
     if #ico_tris > 0 then
         render_mesh(ico_verts,ico_tris,pos1,1,squeeze,t*0.05, -t*0.5, false)
+        if rgb > 0 then
+            render_mesh(ico_verts,ico_tris,pos1,1,squeeze,t*0.05, -t*0.5, false, rgb)
+        end
         if t > 10 then
             zoom= t < 11 and (t-10)*2 or 2
             render_mesh(ico_verts,ico_tris,pos2,zoom,zoom,t*0.1, t*0.3, true)
