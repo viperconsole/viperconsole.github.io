d710ecdb9036ddaf571dbc04634df7e0355e38d1
jice <jice.nospam@gmail.com>
2023-11-25 01:34:28 +0100

    tuto03

---
+++ staging/carts/tuto03.gd
@@ -0,0 +1,21 @@
+var my_font
+func init() :
+	V.gfx.set_spritesheet(V.gfx.load_img("tuto03.png"))
+	V.gfx.set_spritesheet_layout(Vector2i(75,70))
+	my_font=V.gfx.set_font(
+		Vector2i(16, 16),
+		Rect2i(0, 224, 320, 48),
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,'!?-+()\":",
+		Vector2i(1,0),
+		PackedByteArray([16,16,16,16,16,16,16,16,7]))
+
+var t : float = 0.0
+func update() :
+	t += 0.25
+
+func draw() :
+	var sprite = fmod(t, 15.0)
+	V.gfx.clear()
+	V.gfx.blit_sprite(sprite,Vector2(155,42))
+	V.gfx.print(my_font, "HELLO, THIS IS AN OLD SCHOOL SCROLLER ! THANKS FOR WATCHING !...",
+		Vector2(floor(100 - t * 3.5), 112))
