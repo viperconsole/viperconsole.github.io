9b4d948c7c47d06be7d8bb9d76cd057a6b0f85dd
jice <jice.nospam@gmail.com>
2023-12-02 01:40:05 +0100

    soon tag

--- staging/carts/tutos/tuto01.gd
+++ staging/carts/tutos/tuto01.gd
@@ -1,7 +1,6 @@
 func draw() :
 	V.gfx.clear()
-	var t : float = V.elapsed()
-	var x : float = 50 * sin(t)
+	var x : float = 50 * sin(V.elapsed())
 	V.gfx.rectangle(Rect2(140+x, 80, 50, 40), Color8(255, 128, 0))
 	V.gfx.line(Vector2(140, 121), Vector2(240, 121), Color.WHITE)
 	V.gfx.triangle(Vector2(200 + x, 120), Vector2(220 + x, 80), Vector2(240 + x, 120), Color8(128, 0, 255))
